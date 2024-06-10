9949a4d1b7778c7f4f03daa1e68563c3f2da14f7
Bruno Hoinacki <brunohoinackib@gmail.com>
2024-06-10 18:57:04 -0300

    [Feat] ajustando footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,7 +1,8 @@
 <template>
     <footer class="bg-footer text-white p-3 mt-5">
-        <div class="container d-flex justify-content-between align-items-center">
-            <p>&copy; 2024 Seu Nome ou Empresa. Todos os direitos reservados.</p>
+        <div
+            class="container d-flex flex-column flex-md-row justify-content-between align-items-center text-center text-md-start">
+            <p class="small mb-2 mb-md-0">&copy; 2024 Seu Nome ou Empresa. Todos os direitos reservados.</p>
             <div>
                 <a href="https://wa.me/SeuNumero" target="_blank" class="text-white me-3">
                     <font-awesome-icon icon="fa-brands fa-whatsapp" />
@@ -16,3 +17,23 @@
         </div>
     </footer>
 </template>
+
+<style>
+.bg-footer {
+    background-color: #e75480;
+    background-size: cover;
+    background-position: center;
+    color: white;
+    padding: 20px 0;
+    margin-top: 40px;
+}
+
+.bg-footer a {
+    font-size: 1.5rem;
+}
+
+.bg-footer p {
+    margin-bottom: 0;
+    text-shadow: 2px 2px 4px #000000;
+}
+</style>
--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -5,3 +5,40 @@
         </div>
     </header>
 </template>
+<style scoped>
+.bg-header {
+    background-color: #e75480;
+    background-size: cover;
+    background-position: center;
+    color: white;
+    padding: 1rem 0;
+}
+
+.bg-header h1 {
+    text-align: center;
+    font-size: 2rem;
+    text-shadow: 2px 2px 4px #000000;
+}
+
+/* Media query para telas menores que 600px */
+@media (max-width: 600px) {
+    .bg-header {
+        padding: 0.5rem 0;
+    }
+
+    .bg-header h1 {
+        font-size: 1.5rem;
+    }
+}
+
+/* Media query para telas menores que 400px */
+@media (max-width: 400px) {
+    .bg-header {
+        padding: 0.25rem 0;
+    }
+
+    .bg-header h1 {
+        font-size: 1.5rem;
+    }
+}
+</style>
